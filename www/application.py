6d0c9c34d02a650c2ba63393410e08077512b9f3
Lucasca95 <lucasca95@yahoo.com.ar>
2020-05-30 12:44:13 -0300

    mas modelos

--- www/application.py
+++ www/application.py
@@ -39,7 +39,6 @@ class UserHelped(db.Model):
         self.rating = rating
 
     def serialize(self):
-       """Return object data in easily serializeable format"""
        return {
            'id'  : self.id,
            'first_name': self.first_name,
@@ -72,6 +71,96 @@ class ReviewHelped(db.Model):
            'rating': self.rating
         }
 
+class UserCollaborator(db.Model):
+    __tablename__ = 'collaborator'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    birthdate = db.Column(db.DateTime)
+    email = db.Column(db.String(30))
+    password = db.Column(db.String(100))
+    _type = db.Column(db.String(1))
+    rating = db.Column(db.Float)
+    # Relation with Petition
+    petitions = db.relationship('Petition', backref='collaborator')
+    # Relation with ReviewCollaborator
+    reviews = db.relationship('ReviewCollaborator', backref='user')
+
+    def __init__(self, first_name=None, last_name=None, birthdate=None, email=None, password=None, _type=None, rating=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthdate = birthdate
+        self.email = email
+        self.password = password
+        self._type = _type
+        self.rating = rating
+
+    def serialize(self):
+       return {
+           'id'  : self.id,
+           'first_name': self.first_name,
+           'last_name': self.last_name,
+           'birthdate': self.birthdate,
+           'email': self.email,
+           'type': self._type,
+           'rating': self.rating
+        }
+
+class ReviewCollaborator(db.Model):
+    __tablename__ = 'reviewcollaborator'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(140))
+    rating = db.Column(db.Float)
+    # Relation with UserCollaborator
+    user_id = db.Column(db.Integer, db.ForeignKey('collaborator.id'))
+    # Relation with Petition
+    petition_id = db.Column(db.Integer, db.ForeignKey('petition.id'))
+
+    def __init__(self, comment=None, rating=None, gift=None):
+        self.comment = comment
+        self.rating = rating
+
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'  : self.id,
+           'comment': self.comment,
+           'rating': self.rating
+        }
+
+class Petition(db.Model):
+    __tablename__ = 'petition'
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.DateTime)
+    hour = db.Column(db.DateTime)
+    status = db.Column(db.String(20))
+
+    gift = db.Column(db.String(100))
+
+    # Relation with UserHelped
+    helped_id = db.Column(db.Integer, db.ForeignKey('helped.id'))
+    # Relation with UserCollaborator
+    collaborator_id = db.Column(db.Integer, db.ForeignKey('collaborator.id'))
+    
+    # Relation with ReviewHelped
+    review_helped = db.relationship('ReviewHelped', backref='petition')
+    # Relation with ReviewCollaborator
+    review_collaborator = db.relationship('ReviewCollaborator', backref='petition')
+
+
+    def __init__(self, day=None, hour=None, gift=None):
+        self.day = day
+        self.hour = hour
+        self.gift = gift
+
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'  : self.id,
+           'day': self.day,
+           'hour': self.hour,
+           'gift': self.gift
+        }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
